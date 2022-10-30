0e3ad6a90bf68f55f9406c3ee7ba278ea9ccac27
sajims95 <sajims95@gmail.com>
2022-10-30 21:00:30 +0530

    Created function to get write permission

--- app/src/main/java/com/neon/videoer/MainActivity.kt
+++ app/src/main/java/com/neon/videoer/MainActivity.kt
@@ -1,19 +1,66 @@
 package com.neon.videoer
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 import com.neon.videoer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.customBlueNav)
         setContentView(binding.root)
+        requestStoragePermission()
+        setFragment(VideosFragment())
         binding.bottomNav.setOnItemSelectedListener {
-            Toast.makeText(this@MainActivity, "Item Clicked", Toast.LENGTH_SHORT).show()
+            when(it.itemId) {
+                R.id.videoView -> setFragment(VideosFragment())
+                R.id.folderView -> setFragment(FoldersFragment())
+            }
             return@setOnItemSelectedListener true
         }
+
+    }
+
+    // Function for selecting fragments
+    private fun setFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentFL, fragment)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
+    }
+
+    //Requesting permission
+    private fun requestStoragePermission(): Boolean {
+        if(ActivityCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE),7)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 7) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE),7)
+            }
+
+        }
+
     }
 }
