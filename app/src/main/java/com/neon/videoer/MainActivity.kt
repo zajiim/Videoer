20eee46b676b4d3dd24f9a55aa7bf9763c9324be
sajims95 <sajims95@gmail.com>
2022-10-31 12:51:40 +0530

    Added SideNavBar in UI

--- app/src/main/java/com/neon/videoer/MainActivity.kt
+++ app/src/main/java/com/neon/videoer/MainActivity.kt
@@ -4,27 +4,50 @@ import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.neon.videoer.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.customBlueNav)
         setContentView(binding.root)
         requestStoragePermission()
+        toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setFragment(VideosFragment())
+
+
+        //Bottom Nav ClickListener
         binding.bottomNav.setOnItemSelectedListener {
             when(it.itemId) {
                 R.id.videoView -> setFragment(VideosFragment())
                 R.id.folderView -> setFragment(FoldersFragment())
             }
             return@setOnItemSelectedListener true
+        }
+
+        //Side NavBar ClickListener
+        binding.navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.feedbackNav -> Toast.makeText(this, "Feedback", Toast.LENGTH_SHORT).show()
+                R.id.themesNav -> Toast.makeText(this, "Themes", Toast.LENGTH_SHORT).show()
+                R.id.sortOrderNav -> Toast.makeText(this, "Sort", Toast.LENGTH_SHORT).show()
+                R.id.aboutNav -> Toast.makeText(this, "About", Toast.LENGTH_SHORT).show()
+                R.id.exitNav -> exitProcess(1)
+            }
+            return@setNavigationItemSelectedListener true
         }
 
     }
@@ -61,6 +84,15 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+            return true
+        return super.onOptionsItemSelected(item)
 
     }
 }
