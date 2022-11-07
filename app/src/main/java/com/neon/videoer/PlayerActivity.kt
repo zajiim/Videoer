6b11afe08ba08c49e88192b385987fc68b0503d7
sajims95 <sajims95@gmail.com>
2022-11-07 11:47:33 +0530

    Added fullscreen functionality

--- app/src/main/java/com/neon/videoer/PlayerActivity.kt
+++ app/src/main/java/com/neon/videoer/PlayerActivity.kt
@@ -1,10 +1,20 @@
 package com.neon.videoer
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.view.WindowManager
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.exoplayer2.C
+
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+
 
 import com.neon.videoer.databinding.ActivityPlayerBinding
 
@@ -14,12 +24,29 @@ class PlayerActivity : AppCompatActivity() {
          lateinit var player: ExoPlayer
         lateinit var playerList: ArrayList<com.neon.videoer.models.Video>
         var position: Int = -1
+        private var repeat: Boolean = false
+        private var isFullScreen: Boolean = false
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(android.view.Window.FEATURE_NO_TITLE)
+        //For Notch phones above Pie
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        }
         binding = ActivityPlayerBinding.inflate(layoutInflater)
 
+        setTheme(R.style.playerActivityTheme)
         setContentView(binding.root)
+        //For immersive Mode
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, binding.root).let{
+                controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        }
         initializeLayout()
         initializeBinding()
     }
@@ -38,9 +65,13 @@ class PlayerActivity : AppCompatActivity() {
             }
         }
 
+        if(repeat) binding.repeatBtn.setImageResource(com.google.android.exoplayer2.ui.R.drawable.exo_controls_repeat_all)
+        else binding.repeatBtn.setImageResource(com.google.android.exoplayer2.ui.R.drawable.exo_controls_repeat_off)
+
     }
 
     private fun initializeBinding() {
+
         binding.backBtn.setOnClickListener {
             finish()
         }
@@ -54,6 +85,27 @@ class PlayerActivity : AppCompatActivity() {
         binding.prevBtn.setOnClickListener {
             nextPrevVideo(isNext = false)
         }
+        binding.repeatBtn.setOnClickListener {
+            if (repeat) {
+                repeat = false
+                player.repeatMode = Player.REPEAT_MODE_OFF
+                binding.repeatBtn.setImageResource(com.google.android.exoplayer2.ui.R.drawable.exo_controls_repeat_off)
+            } else {
+                repeat = true
+                player.repeatMode = Player.REPEAT_MODE_ONE
+                binding.repeatBtn.setImageResource(com.google.android.exoplayer2.ui.R.drawable.exo_controls_repeat_all)
+            }
+        }
+
+        binding.fullScreenBtn.setOnClickListener {
+            if(isFullScreen) {
+                isFullScreen = false
+                playInFullscreen(false)
+            } else {
+                isFullScreen = true
+                playInFullscreen(true)
+            }
+        }
     }
 
 
@@ -61,6 +113,9 @@ class PlayerActivity : AppCompatActivity() {
 
 
     private fun createPlayer() {
+        try {
+            player.release()
+        } catch (e: Exception) {}
         binding.videoTitle.text = playerList[position].title
         binding.videoTitle.isSelected = true
         player = ExoPlayer.Builder(this).build()
@@ -69,6 +124,14 @@ class PlayerActivity : AppCompatActivity() {
         player.setMediaItem(mediaItem)
         player.prepare()
         playVideo()
+        player.addListener(object : Player.Listener{
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                super.onPlaybackStateChanged(playbackState)
+                //if the video ended
+                if(playbackState == Player.STATE_ENDED) nextPrevVideo()
+            }
+        })
+        playInFullscreen(isFullScreen)
     }
 
     private fun playVideo() {
@@ -88,21 +151,36 @@ class PlayerActivity : AppCompatActivity() {
     }
 
     private fun setPosition(isIncrement: Boolean = true) {
-        if(isIncrement) {
-            if(playerList.size - 1 == position) {
-                position = 0
+        if(!repeat) {
+            if(isIncrement) {
+                if(playerList.size - 1 == position) {
+                    position = 0
+                } else {
+                    ++position
+                }
             } else {
-                ++position
-            }
-        } else {
-            if(position == 0) {
-                position = playerList.size - 1
-            } else {
-                --position
+                if(position == 0) {
+                    position = playerList.size - 1
+                } else {
+                    --position
+                }
             }
         }
 
     }
+
+    private fun playInFullscreen(enable: Boolean) {
+        if(enable) {
+            binding.playerView.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
+            player.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+            binding.fullScreenBtn.setImageResource(R.drawable.fullscreen_exit_icon)
+        }else {
+            binding.playerView.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+            player.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT
+            binding.fullScreenBtn.setImageResource(R.drawable.fullscreen_icon)
+        }
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
